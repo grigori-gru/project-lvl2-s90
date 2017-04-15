552878066b0be48298babc164067faff331aa594
Grigori Grudinin <grgr14@mail.ru>
2017-04-15 14:12:41 +0300

    remove

--- src/option.js
+++
@@ -1,9 +0,0 @@
-import stringify from './options/stringify';
-import plain from './options/plain';
-
-const useFormat = {
-  plain: item => plain(item),
-  stringify: item => stringify(item),
-};
-
-export default (format, data) => useFormat[format](data);
